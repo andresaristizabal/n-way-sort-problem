7f62ad7d517360991a1b424b1b04fe0d046cebbb
Andres Aristizabal <andres@redpanda.com>
2024-09-01 11:23:30 -0500

    verify: add workers for checking order

--- cmd/verify/main.go
+++ cmd/verify/main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"n-way-sort/pkg/utils"
 	"os"
 	"path"
+	"slices"
+	"sync"
 )
+
+func checkOrderFile(file <-chan *os.File, gbPerFile int, group *sync.WaitGroup) {
+	for f := range file {
+		fmt.Println("Checking file: ", f.Name())
+		currentPage := make([]byte, utils.Page)
+		f.ReadAt(currentPage, 0)
+		nextPage := make([]byte, utils.Page)
+		pagePerPage := utils.GB * gbPerFile / utils.Page
+		for i := 0; i < pagePerPage; i++ {
+			f.ReadAt(nextPage, int64(i*utils.Page))
+			if bytes.Compare(currentPage, nextPage) == 1 {
+				panic(fmt.Sprintf("File is not ordered: %s, block: %i", f.Name(), i))
+			}
+			currentPage = slices.Clone(nextPage)
+		}
+		group.Done()
+	}
+}
 
 func main() {
 	folderPath := flag.String("folder-input", "", "input file")
 	gbPerFile := flag.Int("gb-per-file", 2, "number of gb per file")
+	nWorker := flag.Int("n-workers", 1, "number of workers")
 	flag.Parse()
 
 	// read files from folderPath
@@ -19,12 +41,16 @@ func main() {
 	files := make([]*os.File, 0, len(dir))
 	bytesPerFile := int64(*gbPerFile * utils.GB)
 	isThereAnyError := false
+	fileJob := make(chan *os.File, len(dir))
+	waitGroup := sync.WaitGroup{}
+
 	for _, entry := range dir {
 		f, e := os.OpenFile(path.Join(*folderPath, entry.Name()), os.O_RDONLY, 0666)
 		utils.CheckError(e)
 		files = append(files, f)
 	}
 	fmt.Println("Checking file's size")
+
 	for _, f := range files {
 		s, _ := f.Stat()
 		if bytesPerFile != s.Size() {
@@ -33,6 +59,16 @@ func main() {
 		}
 	}
 
+	for i := 0; i < *nWorker; i++ {
+		go checkOrderFile(fileJob, *nWorker, &waitGroup)
+	}
+
+	for _, file := range files {
+		waitGroup.Add(1)
+		fileJob <- file
+	}
+
+	waitGroup.Wait()
 	if !isThereAnyError {
 		fmt.Println("All files are ok")
 	}
